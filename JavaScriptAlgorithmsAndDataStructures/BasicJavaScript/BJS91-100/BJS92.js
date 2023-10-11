9181359565d6b150c3075f49d3d9580414e490e3
Frandede <117942140+Frandede@users.noreply.github.com>
2023-10-11 12:19:31 +0200

    Create BJS92.js

---
+++ JavaScriptAlgorithmsAndDataStructures/BasicJavaScript/BJS91-100/BJS92.js
@@ -0,0 +1,21 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Kendrick Lamar",
+    "title": "DAMN.",
+    "release_year": 2017, 
+    "formats": ["CD", 
+    "Vinyl",
+    "Streaming"], 
+  }
+];
