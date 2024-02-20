e1399543cfc7bb2f3bb9eaa97c8beca7bf7b3a86
Frandede <117942140+Frandede@users.noreply.github.com>
2024-02-20 19:54:07 +0100

    Create ES6_13.js

---
+++ JavaScriptAlgorithmsAndDataStructures/ES6/ES6_11-20/ES6_13.js
@@ -0,0 +1,9 @@
+function removeFirstTwo(list) {
+  // Only change code below this line
+  const [,,...shorterList]=list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
