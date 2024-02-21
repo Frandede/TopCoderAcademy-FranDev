d26e7e5f57a0177947b3fa01357658bf97d4dbad
Frandede <117942140+Frandede@users.noreply.github.com>
2024-02-21 12:42:13 +0100

    Create ES6_15.js

---
+++ JavaScriptAlgorithmsAndDataStructures/ES6/ES6_11-20/ES6_15.js
@@ -0,0 +1,14 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = arr.map(item => `<li class="text-warning">${item}</li>`);
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
