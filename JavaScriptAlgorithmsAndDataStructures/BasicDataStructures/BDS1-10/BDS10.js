701214e7de243506fae301bb71237ef174f127ae
Frandede <117942140+Frandede@users.noreply.github.com>
2024-07-11 13:01:11 +0200

    Create BDS10.js

---
+++ JavaScriptAlgorithmsAndDataStructures/BasicDataStructures/BDS1-10/BDS10.js
@@ -0,0 +1,9 @@
+function quickCheck(arr, elem) {
+  // Only change code below this line
+    if(arr.indexOf(elem)>-1){return true;}
+    else{return false;} 
+    
+  // Only change code above this line
+}
+
+console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
