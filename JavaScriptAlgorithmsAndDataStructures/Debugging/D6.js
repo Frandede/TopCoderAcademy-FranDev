6e794360c19a336c0c5c83a38f2ce60a1c36f93d
Frandede <117942140+Frandede@users.noreply.github.com>
2024-07-11 10:33:45 +0200

    Create D6.js

---
+++ JavaScriptAlgorithmsAndDataStructures/Debugging/D6.js
@@ -0,0 +1,7 @@
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>";
+console.log(innerHtml);
+
+//OTRA SOLUCION
+
+let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
+console.log(innerHtml);
