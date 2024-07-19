06b1cd49672146aee2de3a8a3a966c95193c79da
Frandede <117942140+Frandede@users.noreply.github.com>
2024-07-19 12:13:11 +0200

    Create FP4.js

---
+++ JavaScriptAlgorithmsAndDataStructures/FunctionalProgramming/FP1-10/FP4.js
@@ -0,0 +1,10 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  let value = fixedValue+1;
+  return value;
+
+  // Only change code above this line
+}
