659394c4cc7ae29efd07b502405c6ff3aaf3ae0d
Frandede <117942140+Frandede@users.noreply.github.com>
2024-07-19 12:46:42 +0200

    Create FP5.js

---
+++ JavaScriptAlgorithmsAndDataStructures/FunctionalProgramming/FP1-10/FP5.js
@@ -0,0 +1,9 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(increment) {
+  increment = 1;
+  return increment+fixedValue;
+  // Only change code above this line
+}
