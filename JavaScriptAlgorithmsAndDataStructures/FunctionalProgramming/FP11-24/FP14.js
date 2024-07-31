c9de580e7fc0006d15ab1fa9e91f3e0f154ba53b
Frandede <117942140+Frandede@users.noreply.github.com>
2024-07-31 12:19:44 +0200

    Create FP14.js

---
+++ JavaScriptAlgorithmsAndDataStructures/FunctionalProgramming/FP11-24/FP14.js
@@ -0,0 +1,9 @@
+function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
