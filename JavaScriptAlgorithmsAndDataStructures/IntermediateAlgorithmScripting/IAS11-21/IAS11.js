cdeeab84b95933f9cb00ef1ee039893b7ca0d260
Frandede <117942140+Frandede@users.noreply.github.com>
2024-08-06 12:28:25 +0200

    Create IAS11.js

---
+++ JavaScriptAlgorithmsAndDataStructures/IntermediateAlgorithmScripting/IAS11-21/IAS11.js
@@ -0,0 +1,10 @@
+function convertHTML(str) {
+    return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+convertHTML("Dolce & Gabbana");
