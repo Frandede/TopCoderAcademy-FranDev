4446e2adbbdecb8fade6a3bf0193fbc6997a05d5
Frandede <117942140+Frandede@users.noreply.github.com>
2025-07-22 10:58:35 +0200

    Create react20.jsx

---
+++ FrontEndDevelopmentLibraries/REACT/react20.jsx
@@ -0,0 +1,29 @@
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper/>
+      </div>
+    );
+  }
+};
+// Change code below this line
+
+const Camper = (props) => {
+  return (
+    <div>
+      <p>{props.name}</p>
+    </div>
+  );
+};
+
+Camper.defaultProps = {
+  name: 'CamperBot'
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired
+};
