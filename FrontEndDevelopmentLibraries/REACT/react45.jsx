5954a6678678c95702c815e6c58b7410f56e291f
Frandede <117942140+Frandede@users.noreply.github.com>
2025-07-26 13:31:02 +0200

    Create react45.jsx

---
+++ FrontEndDevelopmentLibraries/REACT/react45.jsx
@@ -0,0 +1,20 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((item, index)=> <li key={index}>{item}</li>); // Change this line
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
